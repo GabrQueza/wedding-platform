79d4dec92b44edf36783aa89184100471a599ee3
Gabriel Quezada <gabrielandradequezada@hotmail.com>
2026-05-21 16:05:57 -0300

    wip - setup inicial do monorepo com turborepo, nestjs e nextjs

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['apps/**', 'packages/**', 'node_modules/**', 'dist/**'],
+  rules: {},
+};
